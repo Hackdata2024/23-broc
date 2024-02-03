ed7484ba8b6c05647199cbeb5efe1720d8cd9456
kunal passan <Kunalpassan30@gmail.com>
2024-02-03 17:06:55 +0530

    feat: readme updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,25 @@
 import Herosection from "@/app/components/herosection";
-import Nav from "@/app/components/Nav";
 import Header from "@/app/components/Header";
+import styles from "@/app/styles/common.module.css"
 
 const Page = () => {
     return (
         <>
             <Header/>
             <Herosection title ={"Our Story"} imageUrl ={"./about1.svg"}/>
+            {/*<section className={styles.medical_checkup}>*/}
+            {/*    <div className={styles.medical_checkup_main}>*/}
+            {/*        <div className={styles.medical_checkup_main_img1}>*/}
+            {/*            <p>hello</p>*/}
+            {/*        </div>*/}
+            {/*        <div className={styles.medical_checkup_main_img1}>*/}
+            {/*            <p>hi</p>*/}
+            {/*        </div>*/}
+            {/*        <div className={styles.medical_checkup_main_img1}>*/}
+            {/*            <p>hola</p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
         </>
     );
 };
--- src/app/components/herosection.js
+++ src/app/components/herosection.js
@@ -37,8 +37,15 @@ const Herosection = ({title,imageUrl}) => {
                         {/*<Image src={imageUrl} alt="image" height={100} width={500}/>*/}
                     </div>
                 </div>
-
             </div>
+            {/*<div className={styles.shape_div}>*/}
+            {/*    <svg className={styles.shape_div_svg} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"*/}
+            {/*         preserveAspectRatio="none">*/}
+            {/*        <path*/}
+            {/*            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"*/}
+            {/*            className="shape-fill"></path>*/}
+            {/*    </svg>*/}
+            {/*</div>*/}
         </main>
     );
 };

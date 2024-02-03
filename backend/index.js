67910eb79a4b043ec54fb6171868a5948dca8df9
mudit <mudit.7.gupta@gmail.com>
2024-02-04 03:32:27 +0530

    database update method & verification

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,7 @@ const { DeployContract } = require("./deploy.js");
 
 const crypto = require("crypto");
 
-const { createHash } = require("crypto");
-
-function generateRandomSHA256Hash() {
-  const randomData = Math.random().toString(36).substring(2, 15);
-
+function generateRandomSHA256Hash(randomData) {
   const sha256Hash = crypto
     .createHash("sha256")
     .update(randomData)
@@ -21,7 +17,7 @@ function generateRandomSHA256Hash() {
   return sha256Hash;
 }
 
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 6000;
@@ -30,16 +26,68 @@ const client = new MongoClient(process.env.MONGO_URI);
 
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-  console.log("We are on buys");
-})
+app.post("/api/formsubmit", async (req, res) => {
+  if (!req.body) {
+    res.status(400).send("Form sent had invalid data");
+    return;
+  }
+  const pres_hash = "0x" + generateRandomSHA256Hash(JSON.stringify(req.body));
+  let id_hash = crypto.randomBytes(32);
+  id_hash = "0x"+ byteToHex(id_hash);
+  const contract_address = await DeployContract(id_hash, pres_hash, 2);
+  // console.log(
+  //   `Smart contract deployed at address ${contract_address} with id ${id_hash} and pres ${pres_hash}`
+  // );
 
-app.post("/api/formsubmit", (req, res) => {
-  console.log(JSON.stringify(req.body));
-  res.send("Hello world");
+  const db = await client.db("Main");
+  const prescription_store = db.collection("Prescription-Data");
+  const address_store = db.collection("Blockchain-addresses");
+  await prescription_store.insertOne({
+    IdentityHash: id_hash,
+    PrescriptionHash: pres_hash,
+  });
+  await address_store.insertOne({
+    IdentityHash: id_hash,
+    ContractAddress: contract_address,
+  });
+  res.send(contract_address);
 });
 
-app.listen(PORT, ()=>{
-  console.log("Server started on port " + PORT);
+app.post("/api/verify", async(req, res)=>{
+  if(!req.body){
+    res.status(400).send("Bad request");
+  }
+  const db = await client.db("Main");
+  const address_store = db.collection("Blockchain-addresses");
+  const x = await address_store.findOne({IdentityHash: req.body.IdentityHash});
+  if(!x){
+    res.status(400).send("Prescription is invalid/does not exist");
+    return;
+  }
+  const hash_from_blockchain = await PrescriptionHash(x.ContractAddress);
+  const pres_hash_store = db.collection("Prescription-Data");
+  const hash_from_db = await pres_hash_store.findOne({IdentityHash: req.body.IdentityHash});
+  if(hash_from_blockchain === hash_from_db.PrescriptionHash){
+    res.status(200).send("Prescription is valid");
+  }else{
+    res.status(400).send("Prescription is invalid");
+  }
 })
 
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
+
+const byteToHex = (byte) => {
+  const key = '0123456789abcdef'
+  let bytes = new Uint8Array(byte)
+  let newHex = ''
+  let currentChar = 0
+  for (let i=0; i<bytes.length; i++) { // Go over each 8-bit byte
+    currentChar = (bytes[i] >> 4)      // First 4-bits for first hex char
+    newHex += key[currentChar]         // Add first hex char to string
+    currentChar = (bytes[i] & 15)      // Erase first 4-bits, get last 4-bits for second hex char
+    newHex += key[currentChar]         // Add second hex char to string
+  }
+  return newHex
+}

d5188cdd184f2f0a1c7932d4d497f94559ae96a3
kunal passan <Kunalpassan30@gmail.com>
2024-02-03 17:41:09 +0530

    feat: color scheme changed

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,7 @@ import styles from "@/app/styles/navbar.module.css";
 import Link from "next/link";
 import Nav from "@/app/components/Nav";
 import Image from "next/image";
-import logo from "../public/Logo.jpeg"
+import logo from "../public/Logo.png"
 
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
                 <div className={styles.main_header}>
                     <div className={styles.navbar_brand}>
                         <Link href="/">
-                            <Image src={logo} alt="mylogo" width={80} height={80}/>
+                            <Image className="px-2" src={logo} alt="mylogo" width={245.5} height={130}/>
                             {/*<Image src="/logo.png" alt="mylogo" width={150} height={40}/>*/}
                         </Link>
                     </div>
